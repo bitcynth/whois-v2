ed14f28acd85d3cb5feb7e1ca2d53d1437777ecc
Cynthia Revstrom <me@cynthia.re>
2019-05-20 21:30:53 +0200

    Initial config data system

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+from app import config_data
+
+config_data.load_data()
 app = Flask(__name__)
 
 from app import routes
---
+++ app/config_data.py
@@ -0,0 +1,28 @@
+import re
+
+WHOIS_SERVER_REGEX = re.compile('^([a-zA-Z0-9_\-]+)\s+([a-zA-Z0-9\-\.]+)')
+ASNS_REGEX = re.compile('^([0-9]+)\-([0-9]+)\s+([a-zA-Z0-9_\-]+)')
+DOMAINS_REGEX = re.compile('^([a-zA-Z0-9\.\-]+)\s+([a-zA-Z0-9_\-]+)')
+
+whois_servers = {}
+domains = {}
+asns = {}
+
+def load_data(asns_file='data/asns.txt', domains_file='data/domains.txt', whois_servers_file='data/whois_servers.txt'):
+    with open(whois_servers_file) as f:
+        for line in f.readlines():
+            match = WHOIS_SERVER_REGEX.findall(line)
+            if len(match) > 0 and len(match[0]) > 1:
+                whois_servers[match[0][0]] = match[0][1]
+    with open(asns_file) as f:
+        for line in f.readlines():
+            match = ASNS_REGEX.findall(line)
+            if len(match) > 0 and len(match[0]) > 2:
+                # TODO: ASN lists
+                #asns[match[0][0]] = match[0][1]
+                pass
+    with open(domains_file) as f:
+        for line in f.readlines():
+            match = DOMAINS_REGEX.findall(line)
+            if len(match) > 0 and len(match[0]) > 1:
+                domains[match[0][0]] = match[0][1]
