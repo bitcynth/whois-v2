29a13f2208abe529be1b646a6bb0e8e1414a961f
Cynthia Revstrom <me@cynthia.re>
2019-05-22 19:19:58 +0200

    Add start page

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,15 @@ from app import app
 from app.whois import query_whois
 from flask import render_template, jsonify, request
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/whois/<path:query>')
 def whois(query):
     print(request.headers['Accept'])
     data = query_whois(query)
-    return render_template('whois_response.html', data=data)
+    return render_template('whois_response.html', data=data, title='WHOIS Result')
 
 @app.route('/api/v1/query_whois', methods=['POST'])
 def api_v1_query_whois():
