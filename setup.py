254a6609f68f9f1a70bb0f828ca947c4bf39a7a0
Cynthia Revstrom <me@cynthia.re>
2019-05-23 20:00:01 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='whois-v2',
-    version='0.0.1',
+    version='0.0.4',
     author='Cynthia Revstrom',
     author_email='me@cynthia.re',
     description='A web based WHOIS client and parser',
@@ -17,5 +17,9 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
-    ]
+    ],
+    install_requires=[
+        'Flask'
+    ],
+    include_package_data=True
 )
