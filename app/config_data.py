678946f783b0fff9533688c5add588b1212dd989
Cynthia Revstrom <me@cynthia.re>
2019-07-15 21:28:55 +0200

    Update config system to support whois servers directly in domains.txt

--- app/config_data.py
+++ app/config_data.py
@@ -2,8 +2,9 @@ import re
 import os
 
 WHOIS_SERVER_REGEX = re.compile('^([a-zA-Z0-9_\-]+)\s+([a-zA-Z0-9\-\.]+)')
-ASNS_REGEX = re.compile('^([0-9]+)\-([0-9]+)\s+([a-zA-Z0-9_\-]+)')
-DOMAINS_REGEX = re.compile('^([a-zA-Z0-9\.\-]+)\s+([a-zA-Z0-9_\-]+)\s+([a-zA-Z0-9_\-]+)')
+ASNS_REGEX = re.compile('^([0-9]+)\-([0-9]+)\s+([a-zA-Z0-9_\-\.]+)')
+DOMAINS_REGEX = re.compile('^([a-zA-Z0-9\.\-]+)\s+([a-zA-Z0-9_\-\.]+)\s+([a-zA-Z0-9_\-]+)')
+COMMENTS_REGEX = re.compile('#.*')
 
 whois_servers = {}
 domains = {}
@@ -19,11 +20,17 @@ def load_data(asns_file='data/asns.txt', domains_file='data/domains.txt', whois_
 
     with open(os.path.join(this_dir, whois_servers_file)) as f:
         for line in f.readlines():
+            line = COMMENTS_REGEX.sub('', line)
+            if line == '':
+                continue
             match = WHOIS_SERVER_REGEX.findall(line)
             if len(match) > 0 and len(match[0]) > 1:
                 whois_servers[match[0][0]] = match[0][1]
     with open(os.path.join(this_dir, asns_file)) as f:
         for line in f.readlines():
+            line = COMMENTS_REGEX.sub('', line)
+            if line == '':
+                continue
             match = ASNS_REGEX.findall(line)
             if len(match) > 0 and len(match[0]) > 2:
                 # TODO: ASN lists
@@ -31,6 +38,9 @@ def load_data(asns_file='data/asns.txt', domains_file='data/domains.txt', whois_
                 pass
     with open(os.path.join(this_dir, domains_file)) as f:
         for line in f.readlines():
+            line = COMMENTS_REGEX.sub('', line)
+            if line == '':
+                continue
             match = DOMAINS_REGEX.findall(line)
             if len(match) > 0 and len(match[0]) > 1:
                 domains[match[0][0]] = DomainEntry(match[0][1], match[0][2])
@@ -40,7 +50,10 @@ def get_server_for_domain(domain):
     for i, d in enumerate(parts):
         dom = '.'.join(parts[i:])
         if dom in domains:
-            return whois_servers[domains[d].server]
+            print(domain)
+            if domains[d].server in whois_servers:
+                return whois_servers[domains[d].server]
+            return domains[d].server
 
 def get_format_for_domain(domain):
     parts = domain.split('.')
